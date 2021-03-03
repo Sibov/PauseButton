1b3964cd94fb2a948125d76c831923db22d9eb39
Home PC <cymonguk@gmail.com>
2021-03-03 09:28:25 +0000

    Add project files.

---
+++ PauseButton/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace PauseButton
+{
+
+    public class Class1 : VTOLMOD
+    {
+        void Update()
+        {
+            if (Input.GetKey("p"))
+            {
+                SetPause();
+            }
+        }
+
+        void SetPause()
+        {
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+            }
+            else
+            {
+                Time.timeScale = 1;
+            }
+        }
+    }
+}
